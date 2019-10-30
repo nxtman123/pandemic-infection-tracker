be162bacaf415b063a06787f63913259bb8b5fb4
Kurtis Jantzen <kurtisjantzen@gmail.com>
2019-10-30 01:25:40 -0600

    Set theme colors

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,9 +1,20 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
+import colors from 'vuetify/lib/util/colors';
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: colors.red.darken1,
+        secondary: colors.lightGreen.darken1,
+      },
+    },
+  },
   icons: {
     iconfont: 'mdi',
   },
