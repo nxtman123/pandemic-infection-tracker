4f374ec68aafa271e44b0f2a2c4debcb44c3c066
Kurtis Jantzen <kurtisjantzen@gmail.com>
2019-10-31 17:03:15 -0600

    Rename pages

--- src/components/DrawerNavigation.vue
+++ src/components/DrawerNavigation.vue
@@ -1,19 +1,19 @@
 <template>
   <v-list>
-    <v-list-item link to="deck">
+    <v-list-item link to="setup">
+      <v-list-item-action>
+        <v-icon>mdi-numeric-1-box</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        Game Setup
+      </v-list-item-content>
+    </v-list-item>
+    <v-list-item link to="infection">
       <v-list-item-action>
         <v-icon>mdi-cards</v-icon>
       </v-list-item-action>
       <v-list-item-content>
         Infection Deck
-      </v-list-item-content>
-    </v-list-item>
-    <v-list-item link to="gameplay">
-      <v-list-item-action>
-        <v-icon>mdi-playlist-edit</v-icon>
-      </v-list-item-action>
-      <v-list-item-content>
-        Gameplay
       </v-list-item-content>
     </v-list-item>
     <v-list-item link to="forecast">
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import InfectionDeck from '@/pages/InfectionDeck.vue';
-import Gameplay from '@/pages/Gameplay.vue';
+import GameSetup from '@/pages/GameSetup.vue';
 import Forecast from '@/pages/Forecast.vue';
 
 Vue.use(VueRouter);
@@ -9,20 +9,20 @@ Vue.use(VueRouter);
 const routes: RouteConfig[] = [
   {
     alias: '/',
-    path: '/deck',
-    name: 'deck',
+    path: '/setup',
+    name: 'setup',
+    components: {
+      default: GameSetup,
+    },
+    meta: { title: 'Game Setup' },
+  },
+  {
+    path: '/infection',
+    name: 'infection',
     components: {
       default: InfectionDeck,
     },
     meta: { title: 'Infection Deck' },
-  },
-  {
-    path: '/gameplay',
-    name: 'gameplay',
-    components: {
-      default: Gameplay,
-    },
-    meta: { title: 'Gameplay' },
   },
   {
     path: '/forecast',
