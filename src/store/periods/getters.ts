b7efdb4d9607d257800d2f04a362cfeea4bf5775
Kurtis Jantzen <kurtisjantzen@gmail.com>
2019-11-02 22:36:03 -0600

    Fix type issue

--- src/store/periods/getters.ts
+++ src/store/periods/getters.ts
@@ -4,23 +4,27 @@ import { RootState } from '@/store/types';
 import { PeriodsState } from './types';
 
 interface Getters extends GetterTree<PeriodsState, RootState> {
-  periods: (state: PeriodsState, _:any, __:any, rootGetters: GetterTree<RootState, RootState>)
+  periods: (state: PeriodsState, _: any, __: any, rootGetters: GetterTree<RootState, RootState>)
     => IPeriod[];
 }
 
 const getters: Getters = {
 
   periods(
-    state: PeriodsState, _:any,
+    state: PeriodsState, _: any,
     rootState: RootState,
   ): IPeriod[] {
     return state.periods.map((cityIds, periodId) => ({
       id: periodId,
-      cards: cityIds.map((cityId, position) => ({
-        ...rootState.infectionDeck.deck[cityId],
-        periodId,
-        position,
-      })),
+      cards: cityIds.map((cityId, position) => {
+        const city = rootState.infectionDeck.deck[cityId];
+        return {
+          cityId: city.id,
+          name: city.name,
+          periodId,
+          position,
+        };
+      }),
     }));
   },
 
